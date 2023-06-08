3837fbf70c45fc1462f413772814c7b0c738c631
tilahundiriba <Tilishdiriba1424@gmail.com>
2023-06-08 23:42:15 +0300

    updated

--- FasiledesGimb/FasiledesGimb/Fasil.cpp
+++ FasiledesGimb/FasiledesGimb/Fasil.cpp
@@ -1,15 +1,22 @@
 #include<windows.h>
 #include<gl/glut.h>
-#include<stdio.h>
-#include<stdlib.h>
 #include<math.h>
+#include<iostream>
+using namespace std;
 
-float tx = -570.0;
-float ty = 600.0;
+
+float tx1m = 1120.0;
+float ty1m= 430.0;
+float tx2m = 1150.0;
+float ty2m = 430.0;
+
+float suntx = -600.0;
+float sunty = 600.0;
+
 void init() {
 	glClearColor(1, 1, 1, 0);
 	glMatrixMode(GL_PROJECTION);
-glOrtho(-280, 600, -200, 600, -300, 300);
+glOrtho(-280, 560, -200, 600, -300, 300);
 	//glOrtho(-60, 40, -60, 120, -50, 50);
 //gluPerspective(60, 1, 0.01, 1000);
 	//gluLookAt(1, 0, 1000, 0, 0, 0, 0, 10, 0);
@@ -23,20 +30,12 @@ glShadeModel(GL_SMOOTH);
 
 glEnable(GL_LIGHTING);
 
-
-
-
 glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
 glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
 glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
 glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
 glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-
-
-
-
 
 glEnable(GL_DEPTH_TEST);
 */
@@ -72,23 +71,70 @@ void keyboard(unsigned char key, int x, int y)
 }*/
 void timer(int w) {
 	glutTimerFunc(1000 / 60, timer, 0);
-	glutPostRedisplay();//calling reagain display function
 	
-	if ((tx <= 1200) && (ty <= 1000)) {
-		tx += 0.05;
-		ty += 0.05;
-	
-	}
-	else if ((ty >= 1000)) {
+	if ((suntx <= 1200) && (sunty <= 1000)) {
+		suntx += 0.2;
+		sunty += 0.2;
 
-		tx += 0.05;
-		ty -= 0.05;
+	}
+	else if ((sunty >= 1000)) {
+
+		suntx += 0.2;
+		sunty -= 0.2;
 
 	}
 	else {
-		tx = -600;
-		ty = 600;
+
+		suntx =-600 ;
+		sunty = 600;
+
 	}
+	
+	glutPostRedisplay();//calling reagain display function
+	
+}
+
+void timer2(int w) {
+	glutTimerFunc(1000 / 60, timer2, 0);
+	if ((tx1m && tx2m >= 980) && (ty1m && tx2m >= -100)) {
+		tx1m -= 0.3;
+		ty1m -= 0.4;
+		tx2m -= 0.3;
+		ty2m -= 0.4;
+
+	}
+
+	else if ((tx1m && tx2m >= 350) && (ty1m && tx2m >= -100)) {
+
+		tx1m -= 0.4;
+		ty1m -= 0.35;
+
+		tx2m -= 0.4;
+		ty2m -= 0.35;
+
+
+	}
+	else if ((ty1m && ty2m <= 50)) {
+		ty1m += 0.4;
+		tx1m -= 0.22;
+		ty2m += 0.4;
+		tx2m -= 0.22;
+
+
+	}
+	else {
+
+		tx1m = 1120;
+		ty1m = 430;
+		tx2m = 1150;
+		ty2m = 430;
+
+	}
+
+	
+	//
+
+	glutPostRedisplay();//calling reagain display function
 }
 void display()
 {
@@ -110,15 +156,7 @@ void display()
 	glVertex2i(1400, 1300);
 	glColor3f(1, 1, 1);
 	glVertex2i(1400, 400);
-	/*glColor3f(0, 0, 0.2);
-	glVertex2i(-600, 400);
-	glColor3f(0, 0, 0);
-	glVertex2i(-600, 1300);
-	glColor3f(0, 0, 0.2);
-	glVertex2i(1400, 1300);
-	glColor3f(0, 0, 0);
-	glVertex2i(1400, 400);*/
-	//the sky color
+
 	glColor3f(0, 0.5, 0);
 	glVertex2i(-600, 400);
 	glVertex2i(-600, -600);
@@ -128,7 +166,7 @@ void display()
 	
 	//sun
 	glPushMatrix();
-	glTranslatef(tx, ty, 0);
+	glTranslatef(suntx, sunty, 0);
 	glColor3f(1, 1, 0);
 	glutSolidSphere(20, 40, 40);
 	glPopMatrix();
@@ -755,18 +793,6 @@ void display()
 	glVertex2i(-30, 150);
 	glVertex2i(0, 150);
 	glVertex2i(0, -50);
-	//the longest supporter left of the smallest strip
-	glColor3f(0.4, 0.2, 0.1);
-	glVertex2i(0, -50);
-	glVertex2i(0, 160);
-	glVertex2i(20, 150);
-	glVertex2i(20, -70);
-	//it's top front cover
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(20, -70);
-	glVertex2i(20, 150);
-	glVertex2i(50, 150);
-	glVertex2i(50, -70);
 
 	//triangle for top front
 	//
@@ -958,17 +984,8 @@ void display()
 	glVertex2i(200, -130);
 	glVertex2i(200, -145);
 	glVertex2i(120, -155);
-	//the left most first supporter
-	glColor3f(0.4, 0.2, 0.1);
-	glVertex2i(20, -70);
-	glVertex2i(20, 70);
-	glVertex2i(80, 50);
-	glVertex2i(80, -170);
-	//the left most second supporter
-	glVertex2i(20, 50);
-	glVertex2i(20, 100);
-	glVertex2i(50, 120);
-	glVertex2i(50, 40);
+	//pick
+	
 	//the front side road
 	glColor3f(0.710, 0.503, 0.334);
 	glVertex2i(130, -150);
@@ -988,111 +1005,12 @@ void display()
 	glVertex2i(1180, 400);
 	glVertex2i(1000, 80);
 
-	//the field door second///////////////
-glColor3f(0.440, 0.387, 0.387);
-glVertex2i(690, -230);//start
-glVertex2i(680, 300);//top
-glVertex2i(770, 250);//2top
-glVertex2i(770, -300);//end
-
-//the front side look of the second door
-glColor3f(0.510, 0.439, 0.439);
-glVertex2i(770, -300);//start
-glVertex2i(770, 250);//top
-glVertex2i(790, 260);//2top
-glVertex2i(790, -280);//end
-//it's top view
-glColor3f(0.610, 0.537, 0.537);
-glVertex2i(695, 310);//start
-glVertex2i(790, 260);//top
-glVertex2i(770, 250);//2top
-glVertex2i(680, 300);//end
 
 
-	//the left front side supporter
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(50, 40);
-    glVertex2i(50, 120);
-	glVertex2i(100, 120);
-	glVertex2i(100, 40);
-//
-	//Color3f(0.2, 0, 0);
-	glVertex2i(80, -170);
-	glVertex2i(80, 40);
-	glVertex2i(130, 40);
-	glVertex2i(130, -170);
-	//strip /derege
-	glColor3f(0.300, 0.255, 0.263);
-	glVertex2i(-30, 50);
-	glVertex2i(0, 50);
-	glVertex2i(0, 35);
-	glVertex2i(-30, 35);
-
-	glColor3f(0.480, 0.432, 0.440);
-	glVertex2i(-30, 35);
-	glVertex2i(0, 35);
-	glVertex2i(5, 25);
-	glVertex2i(-25, 25);
-
-	glColor3f(0.300, 0.255, 0.263);
-	glVertex2i(-25, 25);
-	glVertex2i(5, 25);
-	glVertex2i(5, 10);
-	glVertex2i(-25, 10);
-
-	glColor3f(0.480, 0.432, 0.440);
-	glVertex2i(-25, 10);
-	glVertex2i(5, 10);
-	glVertex2i(10, 0);
-	glVertex2i(-20, 0);
-
-	glColor3f(0.300, 0.255, 0.263);
-	glVertex2i(-20, 0);
-	glVertex2i(10, 0);
-	glVertex2i(10, -15);
-	glVertex2i(-20, -15);
-
-	glColor3f(0.480, 0.432, 0.440);
-	glVertex2i(-20, -15);
-	glVertex2i(10, -15);
-	glVertex2i(15, -25);
-	glVertex2i(-15, -25);
-
-	glColor3f(0.300, 0.255, 0.263);
-	glVertex2i(-15, -25);
-	glVertex2i(15, -25);
-	glVertex2i(15, -40);
-	glVertex2i(-15, -40);
-
-	glColor3f(0.480, 0.432, 0.440);
-	glVertex2i(-15, -40);
-	glVertex2i(15, -40);
-	glVertex2i(20, -50);
-	glVertex2i(-10, -50);
-
-	glColor3f(0.300, 0.255, 0.263);
-	glVertex2i(-10, -50);
-	glVertex2i(20, -50);
-	glVertex2i(20, -65);
-	glVertex2i(-10, -65);
+	//pick
+	
 	//////////////////////////////////////////////////////
-	glColor3f(0.4, 0.2, 0.1);
-	glVertex2i(-30, -50);
-	glVertex2i(-30, 50);
-	glVertex2i(-15, -50);
-	glVertex2i(-15, -85);
 
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(-30, 50);
-	glVertex2i(-20, 50);
-	glVertex2i(-5, -50);
-	glVertex2i(-15, -50);
-
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(-15, -50);
-	glVertex2i(-5, -50);
-	glVertex2i(-5, -85);
-	glVertex2i(-15, -85);
 	//door
 	glColor3f(0.1, 0.1, 0.1);
 	glVertex2i(-20, 50);
@@ -1106,30 +1024,8 @@ glVertex2i(680, 300);//end
 	glVertex2i(-4, 115);
 	glVertex2i(0, 110);
 	//curv for front side 
-	//
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(50, 120);
-	glVertex2i(60, 130);
-	glVertex2i(90, 130);
-	glVertex2i(100, 120);
-	//the yellow one
-	glColor3f(0.4, 0.2, 0.1);
-	glVertex2i(50, 120);
-	glVertex2i(60, 130);
-	glVertex2i(25, 110);
-	glVertex2i(20, 102);
-	//
-	glColor3f(0.430, 0.380, 0.340);
-	glVertex2i(80, 40);
-	glVertex2i(90, 50);
-	glVertex2i(120, 50);
-	glVertex2i(130, 40);
-	//the yellow seconds
-	glColor3f(0.4, 0.2, 0.1);
-	glVertex2i(80, 40);
-	glVertex2i(90, 50);
-	glVertex2i(60, 50);
-	glVertex2i(50, 40);
+	//pick
+
 	//the top left litle home
 	glColor3f(0.300, 0.279, 0.279);
 	glVertex2i(-300, 600);
@@ -1372,6 +1268,8 @@ glVertex2i(680, 300);//end
 	glPopMatrix();
 
 	glBegin(GL_QUADS);
+
+	
 	//the door on the top most left side cylinder
 	glColor3f(0.1, 0.1, 0.1);
 	glVertex2i(-440, 680);
@@ -1469,12 +1367,298 @@ glVertex2i(680, 300);//end
 	glVertex2i(-95, 230);
 	glVertex2i(-60, 230);
 	glEnd();
+	//////
+	  // draw the man's head
+	glPushMatrix();
+	glTranslatef(tx1m, ty1m,0);
+	glScalef(1.2, 1.5, 0);
+	glColor3f(0.620, 0.319, 0.0372);
+	glPushMatrix();
+	glTranslatef(0, 30.7, 0);
+	glutSolidSphere(5.8, 50, 50);
+
+	glEnd();
+	glPopMatrix();
+	glPushMatrix();
+	glBegin(GL_QUADS);
+	//the man neck
+	glColor3f(0.620, 0.351, 0.0992);
+	glVertex2f(-2, 25);
+	glVertex2f(2, 25);
+	glVertex2f(2, 20);
+	glVertex2f(-2, 20);
+	//the man top body
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(-10.3, 20);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(10.3, 20);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(10.5, -20);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-10.5, -20);
+	//the left leg
+	glColor3f(0.420, 0.322, 0.210);
+	glVertex2f(-10.5, -20);
+	glVertex2f(0, -20);
+	glVertex2f(-2, -55);
+	glVertex2f(-8.0, -55);
+	//the right leg
+	glColor3f(0.420, 0.322, 0.210);
+	glVertex2f(0, -20);
+	glVertex2f(10, -20);
+	glVertex2f(8.0, -55);
+	glVertex2f(2, -55);
+	//the left hand
+	glColor3f(0.820, 0.786, 0.754);
+	glVertex2f(-15, 20);
+	glVertex2f(-10, 20);
+	glVertex2f(-13, -20);
+	glVertex2f(-18, -20);
+	//the right hand
+	glColor3f(0.820, 0.786, 0.754);
+	glVertex2f(10, 20);
+	glVertex2f(15, 20);
+	glVertex2f(18, -20);
+	glVertex2f(13, -20);
+	glEnd();
+	glPopMatrix();
 
 	glPopMatrix();
+	
+	// draw the man's head
+	glPushMatrix();
+	glTranslatef(tx2m, ty2m, 0);
+	glScalef(1.2, 1.5, 0);
+	glColor3f(0.620, 0.319, 0.0372);
+	glPushMatrix();
+	glTranslatef(0, 30.7, 0);
+	glutSolidSphere(5.8, 20, 20);
+	glPopMatrix();
+	
+	glPushMatrix();
+	glBegin(GL_QUADS);
+	//the second man neck
+	glColor3f(0.620, 0.351, 0.0992);
+	glVertex2f(-2, 25);
+	glVertex2f(2, 25);
+	glVertex2f(2, 20);
+	glVertex2f(-2, 20);
+	//the second man top body
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(-10.3, 20);
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex2f(10.3, 20);
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex2f(10.5, -20);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex2f(-10.5, -20);
+	//the left leg
+	glColor3f(0.305, 0.400, 0.128);
+	glVertex2f(-10.5, -20);
+	glVertex2f(0, -20);
+	glVertex2f(-2, -55);
+	glVertex2f(-8.0, -55);
+	//the right leg
+	glColor3f(0.305, 0.400, 0.128);
+	glVertex2f(0, -20);
+	glVertex2f(10, -20);
+	glVertex2f(8.0, -55);
+	glVertex2f(2, -55);
+	//the left hand
+	glColor3f(0.820, 0.786, 0.754);
+	glVertex2f(-15, 20);
+	glVertex2f(-10, 20);
+	glVertex2f(-13, -20);
+	glVertex2f(-18, -20);
+	//the right hand
+	glColor3f(0.820, 0.786, 0.754);
+	glVertex2f(10, 20);
+	glVertex2f(15, 20);
+	glVertex2f(18, -20);
+	glVertex2f(13, -20);
+	glEnd();
+	glPopMatrix();
+	glPopMatrix();
+	
+	//////
+	glBegin(GL_QUADS);
+
+	//the longest supporter left of the smallest strip
+glColor3f(0.4, 0.2, 0.1);
+glVertex2i(0, -50);
+glVertex2i(0, 160);
+glVertex2i(20, 150);
+glVertex2i(20, -70);
+//it's top front cover
+glColor3f(0.430, 0.380, 0.340);
+glVertex2i(20, -70);
+glVertex2i(20, 150);
+glVertex2i(50, 150);
+glVertex2i(50, -70);
+//strip /derege
+glColor3f(0.300, 0.255, 0.263);
+glVertex2i(-30, 50);
+glVertex2i(0, 50);
+glVertex2i(0, 35);
+glVertex2i(-30, 35);
+
+glColor3f(0.480, 0.432, 0.440);
+glVertex2i(-30, 35);
+glVertex2i(0, 35);
+glVertex2i(5, 25);
+glVertex2i(-25, 25);
+
+glColor3f(0.300, 0.255, 0.263);
+glVertex2i(-25, 25);
+glVertex2i(5, 25);
+glVertex2i(5, 10);
+glVertex2i(-25, 10);
+
+glColor3f(0.480, 0.432, 0.440);
+glVertex2i(-25, 10);
+glVertex2i(5, 10);
+glVertex2i(10, 0);
+glVertex2i(-20, 0);
+
+glColor3f(0.300, 0.255, 0.263);
+glVertex2i(-20, 0);
+glVertex2i(10, 0);
+glVertex2i(10, -15);
+glVertex2i(-20, -15);
+
+glColor3f(0.480, 0.432, 0.440);
+glVertex2i(-20, -15);
+glVertex2i(10, -15);
+glVertex2i(15, -25);
+glVertex2i(-15, -25);
+
+glColor3f(0.300, 0.255, 0.263);
+glVertex2i(-15, -25);
+glVertex2i(15, -25);
+glVertex2i(15, -40);
+glVertex2i(-15, -40);
+
+glColor3f(0.480, 0.432, 0.440);
+glVertex2i(-15, -40);
+glVertex2i(15, -40);
+glVertex2i(20, -50);
+glVertex2i(-10, -50);
+
+glColor3f(0.300, 0.255, 0.263);
+glVertex2i(-10, -50);
+glVertex2i(20, -50);
+glVertex2i(20, -65);
+glVertex2i(-10, -65);
+glColor3f(0.4, 0.2, 0.1);
+glVertex2i(-30, -50);
+glVertex2i(-30, 50);
+glVertex2i(-15, -50);
+glVertex2i(-15, -85);
+
+glColor3f(0.430, 0.380, 0.340);
+glVertex2i(-30, 50);
+glVertex2i(-20, 50);
+glVertex2i(-5, -50);
+glVertex2i(-15, -50);
+
+glColor3f(0.430, 0.380, 0.340);
+glVertex2i(-15, -50);
+glVertex2i(-5, -50);
+glVertex2i(-5, -85);
+glVertex2i(-15, -85);
+	//the left most first supporter
+	
+	glColor3f(0.4, 0.2, 0.1);
+	glVertex2i(20, -70);
+	glVertex2i(20, 70);
+	glVertex2i(80, 50);
+	glVertex2i(80, -170);
+	//the left most second supporter
+	glVertex2i(20, 50);
+	glVertex2i(20, 100);
+	glVertex2i(50, 120);
+	glVertex2i(50, 40);
+	
+	//the left front side supporter
+	glColor3f(0.430, 0.380, 0.340);
+	glVertex2i(50, 40);
+	glVertex2i(50, 120);
+	glVertex2i(100, 120);
+	glVertex2i(100, 40);
+//
+	//Color3f(0.2, 0, 0);
+	glVertex2i(80, -170);
+	glVertex2i(80, 40);
+	glVertex2i(130, 40);
+	glVertex2i(130, -170);
+	//
+glColor3f(0.430, 0.380, 0.340);
+glVertex2i(50, 120);
+glVertex2i(60, 130);
+glVertex2i(90, 130);
+glVertex2i(100, 120);
+//the yellow one
+glColor3f(0.4, 0.2, 0.1);
+glVertex2i(50, 120);
+glVertex2i(60, 130);
+glVertex2i(25, 110);
+glVertex2i(20, 102);
+//
+glColor3f(0.430, 0.380, 0.340);
+glVertex2i(80, 40);
+glVertex2i(90, 50);
+glVertex2i(120, 50);
+glVertex2i(130, 40);
+//the yellow seconds
+glColor3f(0.4, 0.2, 0.1);
+glVertex2i(80, 40);
+glVertex2i(90, 50);
+glVertex2i(60, 50);
+glVertex2i(50, 40);
+
+	//the field door second///////////////
+	glColor3f(0.440, 0.387, 0.387);
+	glVertex2i(690, -230);//start
+	glVertex2i(680, 300);//top
+	glVertex2i(770, 250);//2top
+	glVertex2i(770, -300);//end
+
+	//the front side look of the second door
+	glColor3f(0.510, 0.439, 0.439);
+	glVertex2i(770, -300);//start
+	glVertex2i(770, 250);//top
+	glVertex2i(790, 260);//2top
+	glVertex2i(790, -280);//end
+	//it's top view
+	glColor3f(0.610, 0.537, 0.537);
+	glVertex2i(695, 310);//start
+	glVertex2i(790, 260);//top
+	glVertex2i(770, 250);//2top
+	glVertex2i(680, 300);//end
+	glEnd();
+	glPopMatrix();
+
 	glFlush();
 
 }
+char ch;
 int main(int argc, char** argv) {
+	std::cout << "          \033[33m______________________________________________________________________________________\033[0m" << std::endl;
+	std::cout << "          \033[33m|////////////////////////////////////GROUP MEMBERS////////////////////////////////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|/////////////////**NAME*****************************ID********************///////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|////////////////**1.MULUKEN GETAYE...............NSR/1584/13*************///////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|////////////////**2.TILAHUN DRIBA................NSR/1584/13*************////////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|////////////////**3.MALAEFIYASH BELETE...........NSR/1372/13*************////////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|//////////////////////PROJECT TITLE GONDER FASILDES//////////////////////////////////|\033[0m" << std::endl;
+	std::cout << "          \033[33m|_____________________________________________________________________________________|\033[0m" << std::endl;
+
+	cout << "              \033[32m|PRESS s|S to Continue and PRESS Enter key!!!\033[0m" << endl;
+	cout << "              \033[32m|PRESS b|B to About the Castel and PRESS Enter key!!!\033[0m" << endl;
+	cout << "              \033[32m|PRESS d|D to Exit and PRESS Enter key!!!\033[0m" << endl;
+	cin >> ch;
+	switch (ch) {
+	case 's':
+	case 'S':
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
 	glutInitWindowPosition(0, 0);
@@ -1485,7 +1669,28 @@ int main(int argc, char** argv) {
 	//glutKeyboardFunc(keyboard);
 	//glutReshapeFunc(reshape);
 	glutTimerFunc(0, timer, 0);
+	glutTimerFunc(0, timer2, 0);
 	//glutKeyboardFunc(timer);
 	glutMainLoop();
+	break;
+	case 'b':
+	case 'B':
+		cout << " \033[35mFasil Ghebbi, also known as the Fasilides Castle, is a fortress-enclosure located in the city of Gondar in Ethiopia.\n\033[0m";
+		cout << " \033[35mIt was built by the Ethiopian emperor Fasilides in the 17th century.";
+		cout << " \033[35mConstruction of the Fasil Ghebbi complex began during the reign of Fasilides in the early 17th century.\n\033[0m";
+		cout << " \033[35mThe Fasilides Castle itself is a large stone structure with multiple levels, towers, and battlements.\n\033[0m";
+		cout << " \033[35mIt is one of the most impressive buildings in the complex and is known for its unique architecture and intricate carvings.\n\033[0m";
+		cout << " \033[35mThe palace was completed and expanded over several generations of Ethiopian emperors.\n\033[0m";
+
+		break;
+	case 'D':
+	case 'd':
+		exit(0);
+		break;
+	default:
+
+		break;
+		
+	}
 	return 0;
 }
